42fff8198c4ac661f8e43310692778d4388a7d27
katyalys <katyalysykh14@gmail.com>
2023-06-18 01:41:31 +0300

    delete comments

--- src/Services/Identity/Identity.WebApi/Helpers/ErrorHandle.cs
+++ src/Services/Identity/Identity.WebApi/Helpers/ErrorHandle.cs
@@ -7,30 +7,6 @@ namespace Identity.WebApi.Helpers
 
     public static class ErrorHandle
     {
-        //public static IActionResult HandleResult<T>(Result<T> result)
-        //{
-        //    if (result.Succeeded)
-        //    {
-        //        return new OkObjectResult(result.Value);
-        //    }
-
-        //    var error = result.Errors[0];
-        //    switch (error.StatusCode)
-        //    {
-        //        case ErrorStatusCode.NotFound:
-        //            return new NotFoundObjectResult(error.Message);
-
-        //        case ErrorStatusCode.WrongAction:
-        //            return new BadRequestObjectResult(error.Message);
-
-        //        case ErrorStatusCode.ForbiddenAction:
-        //            return new ObjectResult(error.Message) { StatusCode = StatusCodes.Status403Forbidden };
-
-        //        default:
-        //            return new StatusCodeResult(StatusCodes.Status500InternalServerError);
-        //    }
-        //}
-
         public static IActionResult HandleResult(Result result)
         {
             if (result.Succeeded)
